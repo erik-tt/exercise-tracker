c579bcadb0030f516f12884a031da1d3f13ba581
Erik Torvbraaten <erik.torvbraten@outlook.com>
2024-12-19 19:28:06 +0100

    stacked line chart for visualization

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
@@ -8,13 +8,56 @@ import withAuth from "@/components/HOC/withAuth";
 import WorkoutTrend from "@/components/workoutTrend";
 import { Card, CardHeader } from "@nextui-org/react";
 import { DateRangePicker } from "@nextui-org/date-picker";
+import { Workout } from "@/types/types";
+import { getWorkouts } from "@/firebase/firestore/getData";
 
-function Page() {
+function Page(this: any) {
+  const { user } = useAuthContext();
+  const uid = user?.uid;
+
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [lastDoc, setLastDoc] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const initialized = useRef(false);
+
+  useEffect(() => {
+    //work around strict mode
+    if (!initialized.current) {
+      initialized.current = true;
+
+      if (loading) {
+        const fetchWorkouts = async () => {
+          try {
+            const { workouts, error } = await getWorkouts("workouts", uid);
+            if (error) {
+              setError("Failed to fetch workouts");
+            } else {
+              setWorkouts((prevWorkouts) => [...prevWorkouts, ...workouts]);
+            }
+          } catch (err) {
+            setError("An error occurred while fetching workouts");
+          } finally {
+            setLoading(false);
+          }
+        };
+        fetchWorkouts();
+      }
+    }
+    setLoading(false);
+  }, [loading, lastDoc, uid]);
+
+  const handleLoadMore = () => {
+    setLoading(true);
+  };
+
+  console.log(workouts)
+
   return (
     <>
-      <div className="mx-8 flex flex-col gap-4 pt-12">
+      <div className="flex flex-col gap-4 pt-12 mx-auto">
         <div className="flex flex-row justify-between px-4">
-          <h1 className="text-4xl text-center pl-4">Workout Analysis</h1>
+          <h1 className="text-4xl text-center pl-4">Analysis</h1>
           <DateRangePicker className="max-w-xs" variant="bordered" />
         </div>
 
@@ -22,8 +65,8 @@ function Page() {
           <Card className="flex-1 p-8 justify-center bg-white shadow rounded-md">
             <CardHeader className="pb-0 pt-2 px-4">
               <div>
-                <h4 className="font-bold text-gray-700 text-3xl">20</h4>
-                <small className="text-gray-500">Total Workouts</small>
+                <h4 className="font-bold text-gray-700 text-3xl">{workouts.length}</h4>
+                <small className="text-gray-500">Total</small>
               </div>
             </CardHeader>
           </Card>
@@ -31,7 +74,9 @@ function Page() {
           <Card className="flex-1 p-8 justify-center bg-white shadow rounded-md">
             <CardHeader className="pb-0 pt-2 px-4">
               <div>
-                <h4 className="font-bold text-gray-700 text-3xl">12</h4>
+                <h4 className="font-bold text-gray-700 text-3xl">
+                  {workouts.filter((workout) => workout.selectedActivity === 'Running').length}
+                </h4>
                 <small className="text-gray-500">Running</small>
               </div>
             </CardHeader>
@@ -40,16 +85,16 @@ function Page() {
           <Card className="flex-1 p-8 justify-center bg-white shadow rounded-md">
             <CardHeader className="pb-0 pt-2 px-4">
               <div>
-                <h4 className="font-bold text-gray-700 text-3xl">8</h4>
-                <small className="text-gray-500">Strength Training</small>
+                <h4 className="font-bold text-gray-700 text-3xl">{workouts.filter((workout) => workout.selectedActivity === 'Weight Lifting').length}</h4>
+                <small className="text-gray-500">Strength</small>
               </div>
             </CardHeader>
           </Card>
         </div>
 
         <div className="bg-white mx-4 p-4 shadow-md rounded-md">
-          <h2 className="font-bold mb-4">Workouts</h2>
-          <WorkoutTrend />
+          <h2 className="font-bold mb-4">Activities by minutes</h2>
+          <WorkoutTrend workoutList={workouts}/>
         </div>
       </div>
     </>
--- src/components/workoutTrend.tsx
+++ src/components/workoutTrend.tsx
@@ -1,28 +1,78 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Workout } from "@/types/types";
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { Area, AreaChart, CartesianGrid, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
-const WorkoutTrend = () => {
-    const [workouts, setWorkouts] = useState<Workout[]>([])
+interface IWorkoutTrendProps {
+    workoutList: Workout[]
+}
 
+const WorkoutTrend: React.FC<IWorkoutTrendProps> = (props: IWorkoutTrendProps) => {
     
-  return(
-    <>
-        <div className="w-full h-64">
-            <ResponsiveContainer width="95%" height="100%">
-                <LineChart width={500} height={400} data={[ {"date": "12/7", "number": 8}, {"date": "15/7", "number": 2}, {"date": "20/7", "number": 12}]}
-                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="date" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="number" stroke="#8884d8" />
-                </LineChart>
-            </ResponsiveContainer>
-        </div>
-    </>
-    );
+    const [data, setData] = useState<any[]>([]);
+    const activityTypes = ["Running", "Cycling", "Weight Lifting", "Skiing", "Roller Skiing", "Hiking", "Walking", "Rowing", "Other"]
+
+    const colors: string[] = [
+        "#8884d8", // Light Purple
+        "#82ca9d", // Soft Green
+        "#ffc658", // Light Orange
+        "#ff7f50", // Coral
+        "#8dd1e1", // Light Blue
+        "#a4de6c", // Light Lime
+        "#d0ed57", // Soft Yellow
+        "#ffbb28", // Warm Yellow
+        "#ff8042", // Vibrant Orange
+    ];
+
+    useEffect(() => {
+        const groupedData: { [key: string]: any} = {};
+
+        props.workoutList.map(workout => {
+            const totalMinutes = Number(workout.hours) * 60 +  Number(workout.minutes);
+            const date = workout.date;
+
+            if (!groupedData[date]) {
+                groupedData[date] = { date };
+                activityTypes.forEach(type => {
+                    groupedData[date][type] = 0;
+                });
+            }
+            groupedData[date][workout.selectedActivity] += totalMinutes;
+        });
+
+        const transformedData = Object.values(groupedData).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+        setData(transformedData);
+    }, [props.workoutList]);
+    
+    return(
+        <>
+            <div className="w-full h-64">
+                <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart width={500} height={400} data={data}
+                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <XAxis dataKey="date" tickMargin={5}/>
+                        <YAxis tickMargin={10} />
+                        <Tooltip />
+                        <Legend />
+                        {activityTypes
+                            .filter(activity => 
+                                // Check if the activity has non-zero values across all data entries
+                                data.some(entry => entry[activity] > 0)
+                            )
+                            .map((activity, index) => (
+                                <Area 
+                                    key={activity} 
+                                    type="monotone" 
+                                    dataKey={activity} 
+                                    stackId="1" 
+                                    stroke={colors[index]} 
+                                    fill={colors[index]}  
+                                />
+                            ))}
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
+        </>
+        );
 };
 
 export default WorkoutTrend;
